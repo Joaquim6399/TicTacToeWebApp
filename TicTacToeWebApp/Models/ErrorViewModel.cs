b1228ea7081f1735f458da0c4e258cd8cbd60910
joaquim6399 <joaquim6399@gmail.com>
2024-05-14 17:48:12 +0100

    Initial commit

---
+++ TicTacToeWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TicTacToeWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
